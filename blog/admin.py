ccb3775c4323922bcb656a648134a4c86a8f00e1
john.dong <dongsijie@agora.io>
2017-12-23 22:54:44 +0800

    add template files

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import BlogArticles
 
-admin.site.register(BlogArticles)
+# admin.site.register(BlogArticles)
+
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ('title','author','publish')
+    list_filter = ('publish','author')
+    search_fields = ('title','body')
+    raw_id_fields = ('author',)
+    date_hierarchy = "publish"
+    ordering = ['publish','author']
 
 
+admin.site.register(BlogArticles,BlogArticlesAdmin)
