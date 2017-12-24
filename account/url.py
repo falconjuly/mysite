a28dc3c140a2d1c10b830048f9774402f90240ea
john.dong <dongsijie@agora.io>
2017-12-24 22:02:41 +0800

    add logout function

--- account/url.py
+++ account/url.py
@@ -2,8 +2,14 @@
 #! -*- coding: utf-8 -*-
 
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
+
+import django.contrib.auth.views
 urlpatterns = [
-    url(r'^$', views.user_login,name="user_login"),
+    # url(r'^$', views.user_login,name="user_login"),
+    url(r'^$', auth_views.login,name="user_login"),
+    # url(r'^logout/$', auth_views.logout,name='user_logout')
+    url(r'^logout/$', auth_views.logout, {"template_name": "account/logout.html"},name='user_logout',)
 ]
