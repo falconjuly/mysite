9704ff4a598e27ed9b17eb0c054eb65aeead6f5c
john.dong <dongsijie@agora.io>
2017-12-24 16:50:52 +0800

    init account

--- account/url.py
+++ account/url.py
@@ -3,8 +3,8 @@
 
 from django.conf.urls import url
 from . import views
+from .views import  user_login
 
 urlpatterns = [
-    url(r'^$', views.blog_title,name="blog_title"),
-    url(r'(?P<article_id>\d)/$',views.blog_article,name="blog_detail")
+    url(r'^$', views.user_login(),name="user_login"),
 ]
